9b224366057965c78f7703bee819db66edf3188d
Raul-TC <raultc.9819@gmail.com>
2022-12-28 03:23:12 -0600

    fix modal image

--- pages/components/Modal.jsx
+++ pages/components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ img, setModal}) => {
     return (
         <div className='fixed top-0 z-20 bg-[#201f1fa9] w-full h-full flex items-center justify-center'>
             <RxCross2 onClick={()=> setModal(false)} className='text-red-500 text-[60px] absolute top-0 mt-8 right-0 mr-4'></RxCross2>
-            <Image className='max-w-[90%] rounded-lg shadow-xl' src={img} width={450} height={450} priority/>
+            <Image className='max-w-[90%] rounded-lg shadow-xl' src={img} width={450} height={450} alt={`image_modal`} priority/>
 </div>    )
 }
 
