38a5ba9eb420ea305a7c982bb4d16e1a524250cb
Raul-TC <raultc.9819@gmail.com>
2023-01-04 23:32:56 -0600

    add automatic slider in single item component

--- pages/item/[id].jsx
+++ pages/item/[id].jsx
@@ -6,12 +6,15 @@ import HeadPage from '../../components/Head'
 import Loader from '../../components/Loader'
 import ThemeContext from '../../context/Theme'
 import { KEY_LOGIN } from '../../KEYS'
+import { IoArrowBackOutline } from "react-icons/io5";
 
 const Page = () => {
     const { DarkTheme } = useContext(ThemeContext)
     let { query: { id } } = useRouter()
     const [dataCharacter, setDataCharacter] = useState([])
     const [loader, setLoader] = useState(true)
+    const [Images, setImages] = useState(0)
+    const [sizeArray, setSizeArray] = useState(0)
     const fetchData = async () => {
         let url = `https://fortniteapi.io/v2/items/get?id=${id}&lang=es`
         const req = await fetch(url,
@@ -26,6 +29,7 @@ const Page = () => {
         if (reqJson.result) {
             setLoader(false)
             setDataCharacter(reqJson.item)
+            setSizeArray(reqJson.item.displayAssets.length)
         }
     }
 
@@ -34,6 +38,36 @@ const Page = () => {
 
 
     }, [id])
+    function resetTimeout(name) {
+        if (name.current) {
+            clearTimeout(timeoutRef.current);
+        }
+    }
+    const timeoutRefImages = useRef(null);
+
+
+
+
+    useEffect(() => {
+        resetTimeout(timeoutRefImages)
+
+
+
+        timeoutRefImages.current = setTimeout(() => {
+
+            if (sizeArray === 0) return
+            if (Images === sizeArray - 1) {
+                setImages(0)
+            } else {
+                setImages(Images + 1)
+            }
+        }, 1500);
+
+
+        return () => {
+            resetTimeout(timeoutRefImages)
+        }
+    }, [Images, dataCharacter])
 
     const [index, setIndex] = useState(0);
     const timeoutRef = useRef(null);
@@ -41,27 +75,26 @@ const Page = () => {
     const delay = 2500;
 
     const [showHistory, setShowHistory] = useState(false)
-    function resetTimeout() {
-        if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-        }
-    }
 
-    useEffect(() => {
-        resetTimeout();
-        timeoutRef.current = setTimeout(
-            () =>
-                setIndex((prevIndex) =>
-                    prevIndex === colors.length - 1 ? 0 : prevIndex + 1
-                ),
-            delay
-        );
-        return () => {
-            resetTimeout();
-        };
-    }, [index, dataCharacter]);
+    // useEffect(() => {
+    //     resetTimeout(timeoutRef);
+    //     timeoutRef.current = setTimeout(
+    //         () =>
+    //             setIndex((prevIndex) =>
+    //                 prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+    //             ),
+    //         delay
+    //     );
+    //     return () => {
+    //         resetTimeout(timeoutRef);
+    //     };
+
+
+    // }, [index, dataCharacter]);
 
     const getDays = (date) => {
+
+        if (!date) return
         let fechaInicio = new Date().getTime();
         let fechaFin = new Date(date).getTime();
 
@@ -72,17 +105,20 @@ const Page = () => {
         return daysOrHours
     }
 
-
+    //  console.info(Images, 'images', sizeArray, "Array Size")
     return (
         <>
             <HeadPage />
             {loader && <div className='min-h-screen flex items-center justify-center'><Loader /></div>}
 
-            {dataCharacter &&
-                < div className='flex flex-col justify-center items-center m-auto max-w-[90%]'>
+            {Object.keys(dataCharacter).length > 0 &&
+                < div className={`${DarkTheme ? 'text-white' : 'text-[#2c2c2c]'} flex flex-col justify-center items-center m-auto w-[90%]`}>
+                    {/* <button className={`${DarkTheme ? ' bg-white text-[#2c2c2c]' : 'bg-[#2c2c2c] text-white'} font-bold py-2 px-4 rounded-md mb-4`}>Regresar</button> */}
+                    <Link href='/' className='self-start'><IoArrowBackOutline className={`${DarkTheme ? 'text-white' : 'text-[#2c2c2c]'} text-5xl mb-4`} /></Link>
                     {/* <h1 className='font-bold text-xl text-center'>{dataCharacter.name}</h1> */}
-                    {/* {dataCharacter.images ? <Image className='rounded-xl' src={dataCharacter.images && dataCharacter.images.full_background} alt={dataCharacter.name} height={350} width={350} /> : <Loader />} */}
-                    <div className="slideshow">
+                    {/* {dataCharacter.images ? <Image key={`Image_${Images}`} className='rounded-xl transition-all ease-out duration-[5s]' src={dataCharacter.displayAssets[Images] ? dataCharacter.displayAssets[Images].full_background : dataCharacter.images.full_background} alt={dataCharacter.name} height={350} width={350} /> : <Loader />} */}
+                    {sizeArray > 1 ? dataCharacter.displayAssets.map((el, index) => <Image width={350} height={350} key={`${el.displayAsset}_${Images}_${index}`} src={el.full_background} className={`${Images === index ? 'block' : 'hidden'}`} alt={`image_${el.displayAsset}_${index}`} />) : <Image src={dataCharacter && dataCharacter.images.full_background} width={350} height={350} alt={dataCharacter.id} />}
+                    {/* <div className="slideshow">
                         <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
                             {dataCharacter.displayAssets && colors.map((backgroundColor, index) => (
                                 <Image
@@ -109,20 +145,30 @@ const Page = () => {
                                 />
                             ))}
                         </div>
-                    </div>
+                    </div> */}
 
-                    <h2 className='text-center'>Apariciones en Tienda:</h2>
+                    {dataCharacter.shopHistory?.length > 3 && <h2 className='text-center font-bold text-2xl'>Apariciones en Tienda:</h2>}
                     <div className='flex flex-col justify-between items-center w-full flex-wrap'>
 
 
-                        <span className='block'>{dataCharacter.shopHistory && dataCharacter.shopHistory[0]},  {getDays(dataCharacter.shopHistory && dataCharacter.shopHistory[0])} </span>
-                        <span className='block'>{dataCharacter.shopHistory && dataCharacter.shopHistory[1]},  {getDays(dataCharacter.shopHistory && dataCharacter.shopHistory[1])} </span>
-                        <span className='block'>{dataCharacter.shopHistory && dataCharacter.shopHistory[2]},  {getDays(dataCharacter.shopHistory && dataCharacter.shopHistory[2])} </span>
+                        <span className='block'>{dataCharacter.shopHistory && dataCharacter.shopHistory[0]}  {getDays(dataCharacter.shopHistory && dataCharacter.shopHistory[0])} </span>
+                        <span className='block'>{dataCharacter.shopHistory && dataCharacter.shopHistory[1]}  {getDays(dataCharacter.shopHistory && dataCharacter.shopHistory[1])} </span>
+                        <span className='block'>{dataCharacter.shopHistory && dataCharacter.shopHistory[2]}  {getDays(dataCharacter.shopHistory && dataCharacter.shopHistory[2])} </span>
 
-                        {showHistory && dataCharacter.shopHistory && dataCharacter.shopHistory.slice(3).map(el => <span className='block transition ease-in-out duration-3000' key={el}>{el}, {getDays(el)}</span>)}
+                        {showHistory && dataCharacter.shopHistory ? dataCharacter.shopHistory.slice(3).map(el => <span className='block transition ease-in-out duration-3000' key={el}>{el} {getDays(el)}</span>) : null}
                     </div>
-                    <button className={`${DarkTheme ? ' bg-white text-[#2c2c2c]' : 'bg-[#2c2c2c] text-white'} w-[60%] h-8 font-bold block mt-4 mb-4 rounded-md text-center`} onClick={() => setShowHistory(!showHistory)}>{showHistory ? 'Ocultar historial' : 'Ver todo el historial'}</button>
-                    <span>{dataCharacter.grants && dataCharacter.grants.map(el => <Link href={`/item/${el.id}`} key={el.id}><Image className='m-auto' src={el.images.full_background} width={350} height={350} alt={el.id} /><p className='font-bold text-center mt-2 text-lg'>{el.description}</p> </Link>)}</span>
+                    {dataCharacter.shopHistory?.length > 3 && <button className={`${DarkTheme ? ' bg-white text-[#2c2c2c]' : 'bg-[#2c2c2c] text-white'} w-[60%] h-8 font-bold block mt-4 mb-4 rounded-md text-center`} onClick={() => setShowHistory(!showHistory)}>{showHistory ? 'Ocultar historial' : 'Ver todo el historial'}</button>}
+                    {/* <span>{dataCharacter.grants && dataCharacter.grants.map(el => <Link href={`/item/${el.id}`} key={el.id}><Image className='m-auto' src={el.images.full_background} width={350} height={350} alt={el.id} /><p className='font-bold text-center mt-2 text-lg'>{el.description}</p> </Link>)}</span> */}
+                    {dataCharacter.grants && dataCharacter.grants.map((el, index) =>
+                        <div key={`${el.id}_${index}`}>
+                            <Link href={`/item/accesorios/${el.id}`}>
+                                <Image className='m-auto' src={el.images.icon_background} width={350} height={350} alt={el.id} />
+                            </Link>
+                            <p>{el.rarity.name}</p>
+                            <p className='font-bold text-center mt-2 text-lg'>{el.description}</p>
+                        </div>
+                    )}
+
 
 
 
---
+++ pages/item/accesorios/[accesorios].jsx
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router'
+import React, { useContext, useEffect } from 'react'
+import ThemeContext from '../../../context/Theme'
+import { KEY_LOGIN } from '../../../KEYS'
+
+const Accesorios = () => {
+    const { DarkTheme } = useContext(ThemeContext)
+    let { query: { accesorios } } = useRouter()
+    // const [dataCharacter, setDataCharacter] = useState([])
+    // const [loader, setLoader] = useState(true)
+    // const [Images, setImages] = useState(0)
+    // const [sizeArray, setSizeArray] = useState(0)
+    const fetchData = async () => {
+        let url = `https://fortniteapi.io/v2/items/get?id=${accesorios}&lang=es`
+        const req = await fetch(url,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': KEY_LOGIN
+                }
+            })
+        const reqJson = await req.json()
+
+        if (reqJson.result) {
+            setLoader(false)
+            setDataCharacter(reqJson.item)
+            setSizeArray(reqJson.item.displayAssets.length)
+            console.info(reqJson)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+
+
+    }, [accesorios])
+
+    // console.info(dataCharacter)
+    return (
+        <div> <h1 className={`${DarkTheme ? 'text-white' : 'text-[#2c2c2c]'} font-bold text-center text-2xl`}>🚧 Página en Construccion 🚧</h1></div>
+    )
+}
+
+export default Accesorios
