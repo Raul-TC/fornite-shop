4ecc9196b6a7b714948d069fa8a3de1d3040f2ca
Raul-TC <raultc.9819@gmail.com>
2023-01-07 03:42:02 -0600

    fix countdown bug, and modify code in Theme.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     const { DarkTheme } = useContext(ThemeContext)
     return (
 
-        <header className='relative z-10 shadow-md  h-[80px] w-full border-c mb-4'>
+        <header className={`${DarkTheme ? 'bg-[#2c2c2c] text-white ' : 'bg-white text-[#2c2c2c]'} relative z-10 shadow-md  h-[80px] w-full border-c mb-4`}>
             <div className='max-w-[1440px] w-[90%] m-auto flex items-center justify-between h-full'>
                 <Link href='/'>
                     <Image src={DarkTheme ? logoTitleWhite : logoTitle} width={150} height={60} alt='logo_fornite' />
