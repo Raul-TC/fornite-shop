36c2259a9213984debb66af11e9930aea3fab2e7
Raul-TC <raultc.9819@gmail.com>
2023-01-07 03:21:29 -0600

    fix countdown bug, and modify code in Theme.jsx

--- context/Theme.jsx
+++ context/Theme.jsx
@@ -9,12 +9,12 @@ const ThemeProvider = ({ children }) => {
   useEffect(() => {
     if (localStorage.getItem('theme') === 'true') {
       setDarkTheme(true)
-      document.body.classList.add('bg-[#2c2c2c]', 'text-white');
-      document.body.classList.remove('bg-white', 'text-[#2c2c2c]');
+      document.body.classList.add('bg-[#2c2c2c] text-white');
+      document.body.classList.remove('bg-white text-[#2c2c2c]');
     } else {
       setDarkTheme(false)
-      document.body.classList.remove('bg-[#2c2c2c]', 'text-white');
-      document.body.classList.add('bg-white', 'text-[#2c2c2c]');
+      document.body.classList.remove('bg-[#2c2c2c] text-white');
+      document.body.classList.add('bg-white text-[#2c2c2c]');
     }
   }, [])
 
@@ -22,13 +22,13 @@ const ThemeProvider = ({ children }) => {
     if (DarkTheme) {
       setDarkTheme(false)
       localStorage.setItem('theme', false)
-      document.body.classList.add('bg-white', 'text-[#2c2c2c]');
-      document.body.classList.remove('bg-[#2c2c2c]', 'text-white');
+      document.body.classList.add('bg-white text-[#2c2c2c]');
+      document.body.classList.remove('bg-[#2c2c2c] text-white');
     } else {
       setDarkTheme(true)
       localStorage.setItem('theme', true)
-      document.body.classList.add('bg-[#2c2c2c]', 'text-white');
-      document.body.classList.remove('bg-white', 'text-[#2c2c2c]');
+      document.body.classList.add('bg-[#2c2c2c] text-white');
+      document.body.classList.remove('bg-white text-[#2c2c2c]');
     }
   }
   const data = { DarkTheme, handleTheme }
