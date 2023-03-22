56020a7064fe7a238b2e2ad64257dd35e2cc3919
Raul-TC <raultc.9819@gmail.com>
2023-03-21 19:52:54 -0600

    modify theme-color tag

--- components/Head.jsx
+++ components/Head.jsx
@@ -2,7 +2,15 @@ import React, { useContext } from 'react'
 import ThemeContext from '../context/Theme'
 import Head from 'next/head'
 
-const HeadPage = ({ title = 'Tienda de hoy Fortnite',darkTheme }) => {
+const HeadPage = ({ title = 'Tienda de hoy Fortnite' }) => {
+  
+  let lsTheme
+  if (typeof window !== "undefined") {
+    
+     lsTheme = window.localStorage.getItem('theme');
+
+
+    }
   return (
     <Head>
       <title>{title}</title>
@@ -13,7 +21,7 @@ const HeadPage = ({ title = 'Tienda de hoy Fortnite',darkTheme }) => {
       <meta property='og:title' content='Fornite Shop Today' />
       <meta property='og:type' content='article' />
       <meta property='og:description' content='Tienda Fornite de Hoy' />
-      <meta name='theme-color' content={darkTheme ? '#2C2C2C' : '#FFFFFF'} />
+      <meta name='theme-color' content={lsTheme === 'true' ? '#2C2C2C' : '#FFFFFF'} />
       {/* <link rel='icon' href='./favicon.ico' /> */}
 <link rel="shortcut icon" href="https://cdn.marketing.on.epicgames.com/fortnite/webpack/../favicon.ico" type="image/x-icon"/>
     </Head>
