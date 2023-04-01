d5314e16e2a111917c80bc548cff5a8c2d0ef3df
Raul-TC <raultc.9819@gmail.com>
2023-03-31 19:58:08 -0600

    fixed date when is new month

--- hooks/useGetDate.js
+++ hooks/useGetDate.js
@@ -5,15 +5,38 @@ export function useDates () {
   const [nextShop, setNextShop] = useState()
 
   const setDay = (date, context) => {
-    let day
-    const month = (date.getMonth() + 1).toString()
+    let day = date.getDate().toString()
+    let month = (date.getMonth() + 1).toString()
     const year = (date.getFullYear()).toString()
+
+    //con 31 dias enero,marzo,mayo,julio,agosto,octubre,diciembre
+    //con 30 Abril,Junio,Septiembre,Noviembre
     let fullDate
+    console.log(month)
     if (date.getHours() >= 18) {
       if (context === 'after') {
-        date.setDate(date.getDate() + 1)
-        day = date.getDate().toString()
+        console.log(month === '3')
+        console.log(month)
+        if ((month === "1" || month === '3' || month === "5" || month === "7" || month === "8" || month === "10" || month === "12") && day === '31') {
+          console.log('estoy aqui pa')
+          date.setMonth(month)
+          month = date.getMonth().toString()
+          // date.setDate(date.getDate())
+          day = date.getDate().toString()
+        } else if ((month === '4' || month === '6' ||month === '9' ||month === '11') && day === '30') {
+           date.setMonth(month)
+          month = date.getMonth().toString()
+          date.setDate(date.getDate())
+          day = date.getDate().toString()
+        }
+        else { 
+          console.log(' es febrero')
+          date.setDate(date.getDate() + 1)
+          day = date.getDate().toString()
+          console.log(day)
+        }
         date.setHours(18, 0, 0, 0)
+console.log('AQKI MEN')
       } else {
         date.setDate(date.getDate())
         day = date.getDate().toString()
@@ -23,7 +46,8 @@ export function useDates () {
       if (context === 'after') {
         date.setDate(date.getDate())
         day = date.getDate().toString()
-        date.setHours(18, 0, 0, 0)
+        console.log('estoy aqki')
+        // date.setHours(18, 0, 0, 0)
       } else {
         date.setDate(date.getDate() - 1)
         day = (date.getDate()).toString()
