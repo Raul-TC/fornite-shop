12974c04aed2d7da74945edd415fb9183577def0
Raul-TC <raultc.9819@gmail.com>
2023-04-01 17:10:00 -0600

    fixed current date when is new month

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -1,67 +1,48 @@
 import Image from 'next/image'
-import React, { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Loader from './Loader'
 
 const ImageSlider = ({ arrayImages }) => {
   const [counter, setCounter] = useState(0)
-   const arraySize = arrayImages.length
+  const arraySize = arrayImages.length
 
-   let bb = useRef(arrayImages)
-   let aa = useMemo(() =>arrayImages, [arrayImages])
-  //  let aa = useMemo(() => bb.current === arrayImages ? bb : bb.current = arrayImages, [])
-   console.info(aa)
-  // const count = useMemo(
-  //   () => 
-  //   {
-      
-  //     setTimeout(() => {
-        
-  //       counter === arraySize - 1
-  //       ? setCounter(0)
-  //       : setCounter(counter + 1)
-  //     }, 1500)
-  //   }
-  //   ,
-  //   [arrayImages],
-  // )
-
-       
+  const aa = useMemo(() => arrayImages, [arrayImages])
 
   useEffect(() => {
-     let ad = setInterval(() => {
-        // if(aa.current === arrayImages) return
-         counter === arraySize - 1
-         ? setCounter(0)
-         : setCounter(counter + 1)
-       }, 2000)
-  
+    const ad = setInterval(() => {
+      // if(aa.current === arrayImages) return
+      counter === arraySize - 1
+        ? setCounter(0)
+        : setCounter(counter + 1)
+    }, 2000)
+
     return () => {
       clearInterval(ad)
     }
   }, [counter])
-  
 
   return (
 
     <div className='h-auto rounded-md relative top-0 w-full'>
       {
-        aa ? aa.map((el, index) =>
-          <Image
-            priority
-            width={150}
-            height={150}
+        aa
+          ? aa.map((el, index) =>
+            <Image
+              priority
+              width={150}
+              height={150}
             // placeholder='blur'
-             blurDataURL='data:image/webp;base64,UklGRrQCAABXRUJQVlA4WAoAAAAgAAAAiAAAiAAASUNDUMgBAAAAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAAAAAAAAAAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADZWUDggxgAAAPAMAJ0BKokAiQA+uUacSzyjoqG81zgDkBcJaW74X6wMT3CHQqt6BAOeMFN40V0SYF2CctZqnhjhl7vuFIFIHLJxOTDwxdbVqFgd6HduAWZ9Mm8ph4Yjkfhw33biEg8zeLN08McPFGr1K+2On/HDvAAA/v3cjPaiN0EdFHnuewGq+/rc07Wt09Jn5Gc3bERtstH8Kj2IQeBkidgSXOLco905usNqG9aUrhIZGfFfAoWXAu7lzbhSdYAisrMuxBF8BHJgvgAAAA=='
+              blurDataURL='data:image/webp;base64,UklGRrQCAABXRUJQVlA4WAoAAAAgAAAAiAAAiAAASUNDUMgBAAAAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAAAAAAAAAAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADZWUDggxgAAAPAMAJ0BKokAiQA+uUacSzyjoqG81zgDkBcJaW74X6wMT3CHQqt6BAOeMFN40V0SYF2CctZqnhjhl7vuFIFIHLJxOTDwxdbVqFgd6HduAWZ9Mm8ph4Yjkfhw33biEg8zeLN08McPFGr1K+2On/HDvAAA/v3cjPaiN0EdFHnuewGq+/rc07Wt09Jn5Gc3bERtstH8Kj2IQeBkidgSXOLco905usNqG9aUrhIZGfFfAoWXAu7lzbhSdYAisrMuxBF8BHJgvgAAAA=='
               key={`_${index}`}
             // src={arrayImages[counter].full_background}
-            src={el.full_background}
-             placeholder='blur'
-            className={`w-full mr-3 md:w-[550px] ${counter === index ? '' : 'w-0 h-0'}   `}
+              src={el.full_background}
+              placeholder='blur'
+              className={`w-full mr-3 md:w-[550px] ${counter === index ? '' : 'w-0 h-0'}   `}
             // src={arrayImages[counter]}
-            alt={`_${index}`}
-          />
-        ) :
-          <Loader/>
+              alt={`_${index}`}
+            />
+          )
+          : <Loader />
       }
     </div>
 
