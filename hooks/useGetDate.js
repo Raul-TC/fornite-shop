72a31ee4ffc56dafd18f32eb217d9f5bdb42ff4e
Raul-TC <raultc.9819@gmail.com>
2023-04-02 18:02:22 -0600

    test fixed date new time zone

--- hooks/useGetDate.js
+++ hooks/useGetDate.js
@@ -70,9 +70,8 @@ export function useDates () {
   }
 
   useEffect(() => {
-    const currentTime = new Date().toLocaleDateString()
-    const nextTime = new Date().toLocaleDateString()
-    console.log(currentTime.toLocaleDateString())
+    const currentTime = new Date()
+    const nextTime = new Date()
     setcurrentShop(setDay(currentTime, 'current'))
     setNextShop(setDay(nextTime, 'after'))
   }, [])
